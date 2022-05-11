e1fc10e1f07e3b7b42006fa766001ca0d05bca92
HimanshuRathee681 <himanshurathee29@gmail.com>
2022-05-11 18:19:51 +0530

    Updates

---
+++ precache-manifest.0e5c912a0b3bc53cc811dc7653e65a93.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c020c0fa8bdeaf9d3c",
+    "url": "/ReactApp/static/css/main.026ec618.chunk.css"
+  },
+  {
+    "revision": "f5c020c0fa8bdeaf9d3c",
+    "url": "/ReactApp/static/js/main.f5c020c0.chunk.js"
+  },
+  {
+    "revision": "6d435980ecb584b13192",
+    "url": "/ReactApp/static/css/1.a759a702.chunk.css"
+  },
+  {
+    "revision": "6d435980ecb584b13192",
+    "url": "/ReactApp/static/js/1.6d435980.chunk.js"
+  },
+  {
+    "revision": "2e2b30ee4b57700ce9f2",
+    "url": "/ReactApp/static/js/runtime~main.2e2b30ee.js"
+  },
+  {
+    "revision": "46113cf09575d41103ef8475eed1b26f",
+    "url": "/ReactApp/static/media/white-hand.46113cf0.png"
+  },
+  {
+    "revision": "6f4b20e84953ec714afa57488dd408b9",
+    "url": "/ReactApp/static/media/rest.6f4b20e8.jpg"
+  },
+  {
+    "revision": "e815618b8167208a95a3add24c2079a7",
+    "url": "/ReactApp/static/media/rest1.e815618b.jpg"
+  },
+  {
+    "revision": "607d8c5c1e2ec6c3842d4465ff2f9a20",
+    "url": "/ReactApp/static/media/rest2.607d8c5c.jpg"
+  },
+  {
+    "revision": "69a5c3173e871bba5ccbb03f19047b0e",
+    "url": "/ReactApp/static/media/rest3.69a5c317.jpg"
+  },
+  {
+    "revision": "16ed31e3a057b75b15ac7e8a593ed0a3",
+    "url": "/ReactApp/static/media/vlad-img.16ed31e3.jpg"
+  },
+  {
+    "revision": "08b712fabaf8b9cf2c537dccc9e887d4",
+    "url": "/ReactApp/static/media/vlad.08b712fa.jpg"
+  },
+  {
+    "revision": "90c43111d6e3f02b477e54a3342d78a1",
+    "url": "/ReactApp/static/media/contact_us.90c43111.jpg"
+  },
+  {
+    "revision": "38c7ac15cc429d67b46f7585c258b3b5",
+    "url": "/ReactApp/static/media/green_burger.38c7ac15.jpg"
+  },
+  {
+    "revision": "f11a6e5a5ae93f01015ccefc2cb29291",
+    "url": "/ReactApp/static/media/birthday.f11a6e5a.jpg"
+  },
+  {
+    "revision": "3b8d7c2407dd4d3f76bff5536321c40a",
+    "url": "/ReactApp/static/media/offerpic.3b8d7c24.jpg"
+  },
+  {
+    "revision": "32ee529ec6bcc4967e5335a7127d1131",
+    "url": "/ReactApp/static/media/offerpic3.32ee529e.jpg"
+  },
+  {
+    "revision": "98a31e019b14f021c970188c87cff3f7",
+    "url": "/ReactApp/static/media/offerpic2.98a31e01.jpeg"
+  },
+  {
+    "revision": "5baccd35a9fb9f7767a6feac7fdcdaba",
+    "url": "/ReactApp/static/media/pizzaParty.5baccd35.jpg"
+  },
+  {
+    "revision": "0c2e4622fcc719c7b374cdafebd66289",
+    "url": "/ReactApp/static/media/jay.0c2e4622.jpg"
+  },
+  {
+    "revision": "18d75ebc9d6bf5886d25dddde754adf0",
+    "url": "/ReactApp/static/media/backPizza.18d75ebc.jpg"
+  },
+  {
+    "revision": "f2174f5fd28a77e49006252b4d40c798",
+    "url": "/ReactApp/static/media/serve.f2174f5f.png"
+  },
+  {
+    "revision": "56cff607150157f60d312e21b61291b4",
+    "url": "/ReactApp/static/media/cart.56cff607.jpg"
+  },
+  {
+    "revision": "d5ab2f42acc1ee66473332ae03dc3dd7",
+    "url": "/ReactApp/static/media/offer.d5ab2f42.png"
+  },
+  {
+    "revision": "ce62dbc1fb005c17d71db4e1cc2679b6",
+    "url": "/ReactApp/index.html"
+  }
+];
